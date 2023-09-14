b2cb21cb6ffc3933aed35498a3cb79c905d384a8
Pragati Srivastava <ps028ideal@gmail.com>
2023-09-14 23:25:33 +0530

    initial

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -4,7 +4,16 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+        <User name="pragati"/>
     </main>
   )
+}
+
+const User=(props)=>
+{
+   return(
+    <di>
+      <h2>hello {props.name}</h2>
+    </di>
+   )
 }
